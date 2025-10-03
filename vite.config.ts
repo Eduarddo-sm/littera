8257e1ec90d27ed6029fb47bc143a8a4064beb6e
adv41 <adv41@cegadv.local>
2025-10-03 17:29:27 -0300

    reajusted vite build to connect to html

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  // Project root for source files
+  base: './',
   root: path.resolve(__dirname, 'src'),
-  // Ensure Vite reads environment files from the repository root (where `.env` is located)
+
   envDir: path.resolve(__dirname),
   build: {
     outDir: path.resolve(__dirname, 'dist'),
@@ -14,6 +14,8 @@ export default defineConfig({
         main: path.resolve(__dirname, 'src/index.html'),
         login: path.resolve(__dirname, 'src/pages/login/login.html'),
         cadastro: path.resolve(__dirname, 'src/pages/cadastro/cadastro.html'),
+        userarea: path.resolve(__dirname, 'src/pages/userarea/userArea.html'),
+        termos: path.resolve(__dirname, 'src/pages/termos/termos.html'),
       },
     },
   },
